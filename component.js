3801ae5288d6fdfb5878046d55bc5e6f187f036c
al-mahfuz <almahfuzhossen@gmail.com>
2023-01-09 22:25:48 +0600

    first commit

---
+++ component.js
@@ -0,0 +1,9 @@
+// there are four types of component.
+
+
+/* 
+1. Similar in look, different in data
+2. Container component
+3. No pattern, But breakdown for working purpose.
+4. Stand-alone component
+*/
