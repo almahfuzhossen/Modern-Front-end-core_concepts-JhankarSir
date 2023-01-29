6cf95235042698f3ec10bea5ddb8b88b434a956e
al-mahfuz <almahfuzhossen@gmail.com>
2023-01-29 19:49:43 +0600

    Js added

---
+++ script.js
@@ -0,0 +1,13 @@
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+
+const displayCountries = countries =>{
+    // console.log(countries[0]);
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(countriesHTML[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML.join(' ');
+}
